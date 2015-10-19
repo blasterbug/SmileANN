f212c3365bf3959cdefffbc6147b699dde351c7b
blasterbug <blasterbug@users.noreply.github.com>
2015-10-19 22:52:00 +0200

    start of something working?

--- faces.py
+++ faces.py
@@ -19,73 +19,116 @@ __version__ = '0.1b'
 
 # useful for stderr output
 #from __future__ import print_function
-import sys, random
-from math import tanh
+import sys
+from math import exp, tanh
+from random import uniform
 
 """
 define a help message
 """
 help = "\nUsage :\n $ python faces.py train facit test\n train: the training set\n facit: the training solution\n test : file for test\n"
 
+def sigmoid( t ) :
+    return 1. / ( 1. + exp( -t ) )
 
-class neuron :
+class Neuron :
     """
-        Define neurons for a Artificial Neural Network
+        Define neurons for a Artificial Neural Network using  the 
+        McCulloch–Pitts (MCP) neuron model.
     """
     # weight of each input
     def __init__( self, size ) :
         """
         Create a neuron
-        :param size: Size of the inputs
+        
+        :param size: Size of the inputs i.e. the number of synapses
         """
         # give randomly weight to each synapses
-        self.synapses = [ random.uniform( -1., 1. ) for i in range( size ) ]
-    
-    def g( self, img ) :
+        self.__synapses__ = [uniform( -1., 1. ) for i in range( size )]
+        
+    def g( self, input_set ) :
         """
         activation function
-        :param img: Image to get the face from
-        :return: face detect by the neuron
+        
+        :param input_set: Input to process
+        :return: activation state of the neuron
         """
         # first compute the sum of the input, regarding weight of synapses
-        for i in range( len( img ) ) :
-            sumInput = img[i] * self.synapses[i]
-        return tanh( sumInput )
+        for i in range( len( self.__synapses__ ) ) :
+            sum_input = input_set[i] * self.__synapses__[i]
+        #return sigmoid( sum_input )
+        return tanh( sum_input)
     
-    def g_training( self, img ) :
+    def learn( self, inputs ) :
         """
-        define a function to train a neuron
+        define a function to set the synpases weight
+        :param value: proportional offset to use to set inputs sensitivity
         """
+        for i in range( len( self.__synapses__ ) ) :
+            self.__synapses__[i] += inputs[i]*0.4
 
-def train( ann, training_set, answers, error_level=40, learning_rate=60 ) :
+class ANN :
     """
-    Train an Artificial Neural Network
-    :param ann: Artificial Neural Network
-    :param training_set: List containing inputs for the training
-    :param answers: list containing the answers for the training set
-    :param error_level=40: Threshold to the error tolerance (in percent)
-    :param learning_rate=60: learning rate for the training (in percent)
+    Define an Aritficial Neuronal Network to recognize faces
     """
-    error = 100
-    sum_error = 0
-    # while the error is "too huge"
-    while error > error_level :
-        # for each inputs
-        for key in training_set :
-            # for each neuron in the network
-            res = [ i for i in range( len( ann ) ) ]
-            for neuron in range( len( ann ) ) :
-                # process the inputs
-                res[neuron] = ann[neuron].g( training_set[key] )
-            # get the highest outputs
-            max_idx =  max(enumerate(res), key=(lambda x: x[1]))
-            error = max_idx[1] - float(answers[key])
-            print( error )
-        error = 0
+    def __init__( self ):
+        self.__ann__ = [Neuron(400) for i in range( 4 )]
+
+    def __perform__( self, image ):
+        """
+        use the ANN to recognize an image
+        
+        :param image: the image to pass through the network
+        :return: enumeration (neuron number, score for the image)
+        """
+        res = [i for i in range( 4 )]
+        for i in range( 4 ):
+            res[i] = self.__ann__[i].g( image )
+        return enumerate(res)
+
+    def train( self, training_set, answers, error_level=45, learning_rate=.4 ) :
+        """
+        Train an Artificial Neural Network
+        
+        :param training_set: List containing inputs for the training
+        :param answers: list containing the answers for the training set
+        :param error_level: Threshold to the error tolerance (in percent)
+        :param learning_rate: learning rate for the training
+        """
+        error = 100
+        # while the error is "too huge"
+        while error > error_level :
+            # for each inputs
+            for key in training_set :
+                # index of the neuron supposed to be activated
+                right_neuron = answers[key] - 1
+                res = self.__perform__( training_set[key] )
+                # get the highest outputs (neurons number in ann and value)
+                better_neuron = max( res, key=( lambda x : x[1] ) )
+                # if the right neuron is not activated
+                if right_neuron != better_neuron[0] : 
+                    # compute the error to the right answer
+                    error = better_neuron[1] - answers[key]
+                    print( better_neuron[0], answers[key], error )
+                    # adjust ann sensitivity according to the error
+                    self.__ann__[ right_neuron ].learn( training_set[key] )
+                else :
+                    print( key, answers[key], better_neuron[0] )
+            error = 0
+        
+    def test( self, test_set ) :
+        """
+        use the ANN
+        
+        :param test_set: test set to be used on the ANN
+        """
+        for img in test_set :
+            print( img, perform( self.__ann__, test_set[img] ) )
 
 def read_images( test_file_name ) :
     """
     Create a dictionary to stored images from a given file
+    
     :param test_file_name: File name which stores the images
     :return: dictionary image_name -> image
     """
@@ -107,22 +150,22 @@ def read_images( test_file_name ) :
                 # then the next lines are the image grey pixels value
                 line = faces_f.readline()
                 # convert string values on the line to integers
-                pixels = [ int( x ) for x in line.split() ]
+                pixels = [int( x ) for x in line.split()]
                 # initiate an array in a vector to put the image in
-                img = [ 0 for i in range( len( pixels) * len( pixels ) )]
+                img = [0 for i in range( len( pixels) * len( pixels ) )]
                 # while the line contains digits
                 for row in range( len( pixels ) ):
                     # convert string values on the line to integers
-                    pixels = [ int( x ) for x in line.split() ]
+                    pixels = [int( x ) for x in line.split()]
                     # matrix in a vector
                     for colum in range( len( pixels ) ):
                         # put the raw of the image in the array
-                        img[ row * len( pixels ) + colum ] = pixels[colum]
+                        img[row * len( pixels ) + colum] = pixels[colum]
                     # then the next lines are the image grey pixels value
                     line = faces_f.readline()
                 line = faces_f.readline()
                 # then link the read image name with the image itself
-                images[ img_name ] = img
+                images[img_name] = img
             line = faces_f.readline()
         return images
     except OSError:
@@ -132,6 +175,7 @@ def read_images( test_file_name ) :
 def read_facit( facit_file_name ):
     """
     Get the answer of a test and store it in a dictionary
+    
     :param facit_file_name: name of the file containing
         answer of the training set
     :return: dictionary storing for each images the answer
@@ -144,16 +188,16 @@ def read_facit( facit_file_name ):
         # for each line
         for line in facit_f :
             # if the line contains a image name and the answer
-            if line.startswith("I"):
+            if line.startswith( "I" ):
                 words = line.split()
                 # create an entry in the dictionary with the image name
                 # and convert to integer the answer associated
-                facit[ words[0] ] = int( words[1] )
+                facit[words[0]] = int( words[1] )
         return facit
     except OSError:
         #print( "Can't open " + test_file_name, file=sys.stderr )
         print( "Can't open " + test_file_name )
-
+    return n.g( img )
 
 if __name__ == "__main__" :
     # require 3 arguments
@@ -167,11 +211,13 @@ if __name__ == "__main__" :
         # get the images for the test
         #test = read_images( sys.argv[3] )
         
-        # create the four neurons with 400 inputs
-        ann = [ neuron( 400 ) for i in range( 4 ) ]
-        
+        # create the ANN
+        ann = ANN()
         # train the network
-        train( ann, training_set, facit )
+        ann.train( training_set, facit )
+        ann.train( training_set, facit )
+        # perform faces recognition
+        #ann.test( test )
         
     else :
         #print( "Bad call\n" + manual, file=sys.stderr )
